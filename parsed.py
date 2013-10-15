4c8de3ae0b0398e79657c8f029eb48ee6895afb0
Johnner <jhnner@gmail.com>
2013-10-16 00:23:19 +0400

    make request

--- parsed.py
+++ parsed.py
@@ -1,13 +1,20 @@
 #coding:utf-8
 
 import requests
-
+VK_audios = 'http://vk.com/audios'
 
 class Parsed:
     '''
     Parser class
     '''
-    def __init__(self):
-        print('hello')
 
-p = Parsed()
+    def __init__(self, vk_id):
+        self.vk_id = str(vk_id)
+        rq = self.make_request()
+        rq.text
+
+    def make_request(self):
+        r = requests.get(VK_audios + self.vk_id)
+        return r
+
+p = Parsed(vk_id=825978)
