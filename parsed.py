9d0dcf7ba5cfa2047dc47648a568812186ae28fb
Johnner <jhnner@gmail.com>
2013-10-17 21:16:45 +0400

    bs upd

--- parsed.py
+++ parsed.py
@@ -1,7 +1,8 @@
 #coding:utf-8
 
 import requests
-import BeautifulSoup as BS
+from BeautifulSoup import BeautifulSoup 
+
 VK_audios = 'http://vk.com/audios'
 
 class Parsed:
@@ -12,10 +13,15 @@ class Parsed:
     def __init__(self, vk_id):
         self.vk_id = str(vk_id)
         rq = self.make_request()
-        rq.text
+        soup = BeautifulSoup(rq.text)
+        print soup.prettify()
+		
         #BeautifulSoup or pyQuery
 
+		
     def make_request(self):
+	'''Request playlist
+	'''
         r = requests.get(VK_audios + self.vk_id)
         return r
 
